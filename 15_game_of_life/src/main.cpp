2757ba558c9e6d3932d30e457880299d26553780
Boaca <emanuel.boaca.ext@siemens.com>
2021-09-23 15:00:42 +0300

    Partial proggress in game of live

--- 15_game_of_life/src/main.cpp
+++ 15_game_of_life/src/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include<vector>
+
+
+#define MINIM_X_SIZE 20;
+#define MINIM_Y_SIZE 20;
+
+int size_x = MINIM_X_SIZE;
+int size_y = MINIM_Y_SIZE;
+std::vector<std::vector<char>> board;
+
+ void setboardSize(int x_size, int y_size)
+{
+	 if (x_size < MINIM_X_SIZE || y_size < MINIM_Y_SIZE)
+	 {
+
+	 }
+}
+
+
 int main()
 {
 	return 0;
