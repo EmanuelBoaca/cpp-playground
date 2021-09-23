56d6a884bf6a86c46c7e3bf89fa49118730d96a7
Boaca <emanuel.boaca.ext@siemens.com>
2021-09-23 08:38:30 +0300

    Am adaugat proectul 05 la solutie

---
+++ 05_show_arguments/src/main_Show_Arguments.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+void showArguments(int nrArg, char* argumentList[])
+{
+	for (int i = 1; i < nrArg; i++)
+	{
+		std::cout << argumentList[i] << std::endl;
+	}
+}
+int main(int argn, char* argv[])
+{
+	showArguments(argn, argv);
+	return 0;
+}
